3e2107b842370ab38d277f8c4edeb3c48ac29ec2
CCoudoin <clement.coudoin@univ-tlse3.fr>
2025-07-24 16:06:33 +0200

    log system added

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -13,6 +13,9 @@
 
 #include <csignal> // For the signal function to exit the while loop with CTRL+C
 
+#include <vector>
+#include <string>
+
 // pour fonction sinusoidale
 #define _USE_MATH_DEFINES
 #include <cmath> //to use sin function
@@ -45,14 +48,52 @@ double sinusoid_movement (double amplitude_max, float t_elapsed)
         return amplitude_max * std::sin(omega*t_sec);
 }
 
+double up_down_movements (double amplitude_max, int iterations)
+{
+	// This function will return a delta that will be given to the send_tool_position
+	// it will be used to do some tests on the robot's response	
+		if (iterations%2 ==0)
+		{
+			return amplitude_max;
+		} else
+		{
+			return - amplitude_max;
+		}
+	
+}
+
+// This struct is used to log data into a file.
+struct LogEntry {
+	double timestamp;
+	double delta;
+};
+
+// This struct is used to log errors
+struct ErrorLogEntry{
+	double timestamp;
+	std::string message;
+};
+
+double get_current_time()
+{
+    struct timeval current;
+    gettimeofday(&current, nullptr);
+    return current.tv_sec + current.tv_usec * 1e-6;
+}
+
 int main()
 {
-	
+	//start time
+	double start_time = get_current_time();
+
+	// struct used to log errors
+	std::vector<ErrorLogEntry> error_log;
+
+	// struct used to log data
+	std::vector<LogEntry> data_log;
+
 	// To interupt with CTRL+C
 	signal(SIGINT, sigHandler);
-	// log file
-	std::ofstream logfile("cmd_trajectory.csv");
-	logfile << "time,dz"<<std::endl;
 
 	// creation of the socket and its configuration (struct)
 	SocketConfig cfg;
@@ -60,7 +101,8 @@ int main()
 	try {
 		sock.connect();
 	} catch (const std::exception& e){
-		std::cerr << "Failed to connect: " << e.what() << std::endl;
+		// std::cerr << "Failed to connect: " << e.what() << std::endl;
+		error_log.push_back({get_current_time(), "[ERROR] Failed to connect: " + std::string(e.what())});
 	} 
 	
 	// Command of the robot
@@ -73,10 +115,9 @@ int main()
 			cmd.init_driver(sock);
 			cmd.control_mode(sock,ControlMode::jogging);
 	} catch (const std::exception& e){
-		std::cerr << "main : command preparation failed : " << e.what() << std::endl;
+		//std::cerr << "main : command preparation failed : " << e.what() << std::endl;
+		error_log.push_back({get_current_time(),"[ERROR] Failed command preparation: " + std::string(e.what())});
 	}
-
-	auto total_running_time = std::chrono::steady_clock::now();
 	while(keep_running)
 	{
 		auto loop_start = std::chrono::steady_clock::now();
@@ -89,20 +130,36 @@ int main()
 		}*/
 
 		//===parameters for the sinusoidal mouvement===
-		double amplitude_max = 0.005; // displacement of 5mm
-		auto command_time = std::chrono::steady_clock::now();
-		auto t_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(command_time - total_running_time).count();
+		double amplitude_max = 0.005; // unit is m -> displacement of 5mm
+		auto command_time = get_current_time();
+		auto t_elapsed = command_time - start_time;
 			// ============================================
 
 		try {
 
-			double dz = sinusoid_movement(amplitude_max,t_elapsed);
-			logfile << t_elapsed << "," << dz << std::endl;
-			cmd.send_tool_position(Direction::z,sock,dz);
+			double delta = sinusoid_movement(amplitude_max,t_elapsed);
+
+			// logfile << t_elapsed << "," << dz << std::endl;
+			data_log.push_back({get_current_time(),delta});
+
+			cmd.send_tool_position(Direction::z,sock,delta);
 
 		} catch (const std::exception& e){
-				std::cerr << "sending of tool position failed : " << e.what() << std::endl;
+				// std::cerr << "sending of tool position failed : " << e.what() << std::endl;
+				error_log.push_back({get_current_time(),"[ERROR] Tool position sending failed : " + std::string(e.what())});
 		}
+
+		/* A voir comment je gère le sleep dans ce cas.. A creuser
+		try {
+			for (int i=0; i<10; ++i)
+			{
+				double dz = up_down_movements(0.005,i);
+				cmd.send_tool_position(Direction::z,sock,dz);
+
+			}
+		} catch (const std::exception& e){
+			std::cerr << "sending of tool position failed : " << e.what() << std::endl;
+		}*/
 		
 		/*try {
 			std::cout << "========tool position (after)=========" << std::endl;
@@ -120,19 +177,35 @@ int main()
 			std::this_thread::sleep_for(std::chrono::milliseconds(4 - duration.count()));// We want 250Hz which is 4ms
 		} else
 		{
-			std::cout << "WARNING: loop higher than expected duration!" << std::endl;
+			// std::cout << "WARNING: loop higher than expected duration!" << std::endl;
+			error_log.push_back({get_current_time(),"[WARNING] loop duration higher than expected duration!"});
 		}
 
-		std::cout << "Duration : " << duration.count() << " milliseconds" << std::endl;
-
 	}
+
 
 	try {
 		cmd.reset_driver(sock);
 		sock.close();
 	} catch (const std::exception& e){
-			std::cerr << "ending of the programm failed : " << e.what() << std::endl;
+			// std::cerr << "ending of the programm failed : " << e.what() << std::endl;
+			error_log.push_back({get_current_time(),e.what()});
 	}
-		
+
+	// log file data
+	std::ofstream logfile("cmd_trajectory.csv");
+	for (const auto& e : error_log)
+	{
+		logfile << "[" << e.timestamp << "s] " << e.message << "\n";
+	}
+
+
+	// log file errors
+	std::ofstream err_file("log_errors.txt");
+	for (const auto& e : error_log)
+	{
+		err_file << "[" << e.timestamp << "s] " << e.message << "\n";
+	}
+	
 
 }
